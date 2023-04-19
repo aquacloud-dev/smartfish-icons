89a6ae781b451adaf687d6f990278c5442871205
rawnly <rawnly@users.noreply.github.com>
2023-04-19 13:10:50 +0200

    feat: new export, reduced package size

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,9 @@ export default defineConfig({
 	sourcemap: false,
 	minify: true,
 	dts: true,
+	entry: ["src/**.tsx"],
 	clean: true,
+	treeshake: true,
 	name: "Smartfish Icons",
 	format: ["esm", "cjs"],
 	loader: {
